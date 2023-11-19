69bea9116a831ff546542975ef6d395c0d7dc708
DalanDan <jet320boy@gmail.com>
2023-11-19 13:02:17 +0800

    Add comments that come from the book

--- Professional C++, Fifth Edition (Marc Gregoire)/A Crash Course in C++ and the Standard Library/5_namespaces.cpp
+++ Professional C++, Fifth Edition (Marc Gregoire)/A Crash Course in C++ and the Standard Library/5_namespaces.cpp
@@ -17,4 +17,14 @@ using namespace mycode;
     Most code snippets in this book assume a 'using' directive
     for the std namespace so that everything from the C++ Standard
     Library is can be used without the need of the std:: prefix.
+
+    WARNING:
+        Never put a 'using' directive in a header file at global
+        scope. This will pollute the global namespace of any file that 
+        includes the header file. Putting it on a smaller scope, for instance
+        at 'namespace' or 'class scope', is acceptable, even in a header.
+        It's also perfectly fine to put a 'using' directive or declaration
+        in a module interface file, as long as you don't export it. However,
+        this book always fully qualifies all types in module interface files,
+        as I think it makes it easier to understand an interface.
 */
